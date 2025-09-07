291bea68d67be7f05ca126765fc0d8d7cdb74411
Mokhtar Ebrahim <mokhtarali2001@yahoo.com>
2025-09-07 23:09:25 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    crypto.randomUUID(),
+    siteToken: crypto.randomUUID(),
     redirectStatusCode: '301',
     linkCacheTtl: 60,
     redirectWithQuery: false,
